17de94e3aa154f5bea695cceb10d742c6af6571c
ANNA <ANNA@DESKTOP-V04LNJV>
2016-10-08 21:03:51 +0300

    Добавьте файлы проекта.

---
+++ FindDublicates/DuplicatesFinder.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDublicates
+{
+    class DuplicatesFinder
+    {
+        // All dublicates of start directory
+        Dictionary<string, List<string>> dublicates;
+        // Errors when opening directory without access violation
+        int countError;
+
+        public int ErrorDuringLookingForDuplicates
+        {
+            get { return countError; }
+            set { countError = value; }
+        }
+
+        public DuplicatesFinder()
+        {
+            dublicates = new Dictionary<string, List<string>>();
+            countError = 0;
+        }
+
+        // Looking for dublicates in current directory
+        public void GetFilesDuplicates(string path)
+        {
+            Dictionary<long, List<string>> filelist = new Dictionary<long, List<string>>();
+            var dir = new DirectoryInfo(path);
+            try
+            {
+                // Group all files by length of files
+                filelist = dir.GetFiles("*.*", SearchOption.AllDirectories)
+                              .AsParallel()
+                              .GroupBy(t => t.Length)
+                              .ToDictionary(y => y.Key, y => y.Select(f => f.FullName).ToList());
+
+                // Looking for at files with same length files which has same hashes
+                Parallel.ForEach(filelist.Where(it => it.Value.Count > 1), (currentgroup) =>
+                {
+                    Parallel.ForEach(currentgroup.Value, (filename) =>
+                    {
+                        using (var md5 = MD5.Create())
+                        {
+                            using (var stream = File.OpenRead(filename))
+                            {
+                                byte[] checkSum = md5.ComputeHash(stream);
+                                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                                if (dublicates.ContainsKey(result))
+                                    lock (dublicates) { dublicates[result].Add(filename); }
+                                else
+                                    lock (dublicates) { dublicates.Add(result, new List<string> { filename }); }
+                            }
+                        }
+                    });
+                });
+
+            }
+            catch (Exception)
+            {
+                ErrorDuringLookingForDuplicates ++;
+            }
+        }
+
+        // Looking for dublicates in current path
+        public Dictionary<string, List<string>> Find(string path)
+        {
+            dublicates = new Dictionary<string, List<string>>();
+            countError = 0;
+
+            // if getting path to logical drive
+            if (Path.GetDirectoryName(path) == null)
+            {
+                var drive = new DirectoryInfo(path);
+
+                // Looking for dublicates in each directory of locical drive
+                Parallel.ForEach(drive.GetDirectories(), (currentdir) =>
+                {
+                    GetFilesDuplicates(currentdir.FullName);
+                });
+            }
+            else
+                GetFilesDuplicates(path);
+
+            return dublicates;
+        }
+
+        // Write dublicates to report file
+        public void WriteReportToStream(string filepath)
+        { 
+            using (StreamWriter file = new StreamWriter(filepath))
+            {
+                foreach (var hash in dublicates)
+                {
+                    if (hash.Value.Count > 1)
+                    {
+                        foreach (var dubfile in hash.Value)
+                        {
+                            file.WriteLine("{0}: {1}", hash.Key, dubfile);
+                        }
+                        file.WriteLine();
+                    }
+                }
+                if (ErrorDuringLookingForDuplicates > 0)
+                    file.WriteLine("{0} missed directory by reason of an access violation", ErrorDuringLookingForDuplicates);
+            }
+        }
+
+    }
+}
---
+++ FindDublicates/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDublicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DuplicatesFinder dublicates = new DuplicatesFinder();
+
+            Console.Write("Command 'exit' or directory> ");
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "exit")
+            {
+                // Ignoring invalid input
+                if (!new DirectoryInfo(command).Exists)
+                {
+                    Console.WriteLine("Path does not exist!");
+                }
+                else
+                {
+                    Console.WriteLine("Waiting...");
+                    var start = DateTime.Now;
+                    var dubFiles = dublicates.Find(command);
+                    int countRecords = dubFiles.Count;
+
+                    // Getting name of report file
+                    string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string filepath = string.Format("dublicates {0}.txt", start.ToString("yyyy.MM.dd-HH.mm.ss"));
+                    filepath = Path.Combine(mydocpath, filepath);
+
+                    dublicates.WriteReportToStream(filepath);
+
+                    // if input is too much, write dublicates only to report file
+                    if (countRecords > 50 )
+                        Console.WriteLine("Report of finding dublicates write to file {0}", filepath);
+                    // write dublicates to console
+                    else
+                    {
+                        foreach (var hash in dubFiles)
+                        {
+                            if (hash.Value.Count > 1)
+                            {
+                                foreach (var dubfile in hash.Value)
+                                {
+                                    Console.WriteLine("{0}: {1}", dubfile);
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        Console.WriteLine("Report of finding duplicates also write to file {0}", filepath);
+                    }
+
+                    // Show if we get some error looking for dublicates in directory
+                    if (dublicates.ErrorDuringLookingForDuplicates > 0)
+                        Console.WriteLine("{0} missed directory by reason of an access violation", 
+                                                                          dublicates.ErrorDuringLookingForDuplicates);
+
+                    var finish = DateTime.Now;
+                    Console.WriteLine("Executing time: {0}", finish.Subtract(start));
+                }
+
+                Console.Write("\nCommand 'exit' or directory> ");
+                command = Console.ReadLine();
+
+            }
+        }
+    }
+}
